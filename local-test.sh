d1eac7d3d4aad3b872c5484af5a07ea69a2a9c9f
Ibrahim Umar <iambaim@users.noreply.github.com>
2024-07-10 15:13:35 +0200

    Run local test

--- local-test.sh
+++ local-test.sh
@@ -3,13 +3,25 @@ set -euo pipefail
 
 # Build
 export CGO_ENABLED=1
+
+# For github actions
+if [[ ! -z ${GITHUB_RUN_ID+y} ]]; then
+  export MQ_INSTALLATION_PATH=$HOME/IBM/MQ/data
+  export CGO_CFLAGS="-I$MQ_INSTALLATION_PATH/inc"
+  export CGO_LDFLAGS="-L$MQ_INSTALLATION_PATH/lib64 -Wl,-rpath,$MQ_INSTALLATION_PATH/lib64"
+  echo $CGO_LDFLAGS
+fi
+
 go install go.k6.io/xk6/cmd/xk6@latest
 xk6 build \
     --with github.com/iambaim/xk6-ibmmq=.
 
 # Run dev MQ container and wait until MQ is ready
 docker-compose -f example/docker-compose.yml up -d localmqtest
-sleep 10
+while curl --output /dev/null --silent --head --fail localhost:1414 ; [ $? -ne 52 ];do
+  printf '.'
+  sleep 5
+done
 
 # Run tests
 export MQ_QMGR="QM1"
